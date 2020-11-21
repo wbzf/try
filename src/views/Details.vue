26e6040325162e548463283c3827f5e45185a845
wbzf <2428935270@qq.com>
2020-11-21 22:23:49 +0800

    test

---
+++ src/views/Details.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="top">
+    <span>记事本</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default class Details extends Vue{
+  name: "Details" | undefined
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
